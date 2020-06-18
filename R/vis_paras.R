b2415b789693378b31c9a29f23dfafa9cff982f9
frank0434 <jian.liu@plantandfood.co.nz>
2020-06-18 16:03:29 +1200

    a new subset fun for filtering observed data; impove vis fun to cover profile and layer

--- R/vis_paras.R
+++ R/vis_paras.R
@@ -1,6 +1,25 @@
 
-
-#' subset_toPlot
+#' subset_obs
+#' @description subset the obersvation data based on treatment
+#'
+#' @param DT
+#' @param col_treatment1
+#' @param col_treatment2
+#' @param treatment1
+#' @param treatment2
+#'
+#' @return
+#' @export
+#'
+#' @examples
+subset_obs <-  function(DT = SW_mean,
+                        col_treatment1 = "Experiment",
+                        col_treatment2 = "SowingDate",
+                        treatment1, treatment2){
+  DT = DT[get(col_treatment1) == treatment1 & get(col_treatment2) == treatment2]
+  DT
+}
+#' subset_pred
 #'
 #' @param DT
 #' @param col_treatment1
@@ -13,7 +32,7 @@
 #' @export
 #'
 #' @examples
-subset_toPlot <- function(DT = top5,
+subset_pred <- function(DT = top5,
                           col_treatment1 = "Experiment",
                           col_treatment2 = "SowingDate",
                           treatment1, treatment2){
@@ -27,10 +46,13 @@ subset_toPlot <- function(DT = top5,
 #' plot_params
 #'
 #' @description
+#'
 #' @param DT
-#' @param treatment1
-#' @param treatment2
 #' @param title
+#' @param col_pred
+#' @param col_obs
+#' @param point_size
+#' @param Depth default is profile. can be set the "Depth" to check each layer.
 #'
 #' @import ggplot2
 #' @import stringr
@@ -39,30 +61,42 @@ subset_toPlot <- function(DT = top5,
 #' @export
 #'
 #' @examples
-plot_params <- function(DT = top5, title, point_size = 2){
+plot_params <- function(DT = top5,
+                        title,
+                        col_pred = "PSWC",
+                        col_obs = "SWC",
+                        point_size = 2,
+                        Depth = NULL,
+                        width = 10, height = 10){
   palette = rep("grey", times = 2)
-  palette_named = setNames(palette,  c("Predict SWC", "Observed SWC"))
+  palette_named = setNames(palette,  c(col_pred, col_obs))
   palette_named[2] = "red"
+  if(is.null(Depth)){
+    facet_form =  reformulate(".", "KLR_RFV_SKL" )
+  } else if(Depth == "Depth"){
+    facet_form =  reformulate("KLR_RFV_SKL", "Depth" )
+
+  }
   if(is.null(dim(DT)) | nrow(DT) == 0){
     print(paste0(DT, " is empty!!!"))
   } else {
 
       DT %>%
         ggplot(aes(Date)) +
-        geom_point(aes(y = PSWC, color = names(palette_named)[1]), size = point_size) +
-        geom_point(aes(y = SWC,color = names(palette_named)[2]), size = point_size) +
-        geom_text(aes(x = max(DT$Date) - 60, y = median(DT$PSWC),
+        geom_point(aes(y = get(col_pred), color = names(palette_named)[1]), size = point_size) +
+        geom_point(aes(y = get(col_obs), color = names(palette_named)[2]), size = point_size) +
+        geom_text(aes(x = as.Date("2011-07-01"), y = median(DT[[col_pred]]),
                       label = paste0("NSE = ", NSE,
                                      "\r\nR.square = ", R2,
                                      "\r\nRMSE = ", RMSE)),inherit.aes = FALSE)+
-        facet_grid(  KLR_RFV_SKL ~.) +
+        facet_grid( facet_form) +
         ggtitle(paste0(unique(DT$Experiment), unique(DT$SowingDate))) +
         scale_x_date(date_labels = "%Y %b", date_breaks = "4 weeks") +
         scale_color_manual(name = "", values = palette_named) +
         theme_water() +
         theme(legend.position = "top",
               axis.text.x = element_text(angle = 30, hjust = 1)) +
-        ggsave(stringr::str_glue("{title}.pdf"), width = 10, height = 10, dpi = 300)
+        ggsave(stringr::str_glue("{title}.pdf"), width = width, height = height, dpi = 300)
     }
 
 }
