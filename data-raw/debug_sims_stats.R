3210af2f86e5e17d46435bfeb920e25e2838d9b8
frank0434 <jian.liu@plantandfood.co.nz>
2020-07-02 09:32:08 +1200

    Move a debug code to Rbuildignore

--- data-raw/debug_sims_stats.R
+++ data-raw/debug_sims_stats.R
@@ -7,12 +7,12 @@ col_treatment1 = "Experiment"
 col_treatment2 = "SowingDate"
 mode = "Manual"
 keys =  c("Experiment", "SowingDate", "Depth")
-# DT_observation = readRDS("./data/SW_mean.rds")
+DT_observation = SW_mean
 keys = c("Experiment", "SowingDate", "Depth")
 col_pred = "pred_VWC"
 col_obs = "ob_VWC"
 # library(data.table)
-# l_stats_layerKL = autoapsimx::sims_stats_multi(path_sims = "C:/Data/Master/03processed-data/apsimxFilesLayers/",
+# l_stats_layerKL = sims_stats_multi(path_sims = "C:/Data/Master/03processed-data/apsimxFilesLayers/",
 #                                                pattern = "^SKL.+.db$",
 #                                                DT_observation = DT_observation,
 #                                                mode = "Manual",
