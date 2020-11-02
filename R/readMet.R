7c1777c4e031fac89d042a9655fda7e6ad9b2533
frank0434 <jian.liu@plantandfood.co.nz>
2020-11-02 16:19:05 +1300

    Process met file with fiter

--- R/readMet.R
+++ R/readMet.R
@@ -26,17 +26,32 @@ read_met_col <- function(path = path_met, skip = 7){
 #' @param path A character string. The path to access the met files.
 #' @param skip_unit An integer. The number of rows for skipping the unit line in met files.
 #' @param skip_meta An integer. The number of rows for skipping the meta data before the column names start.
+#' @param startd
+#' @param endd
+#' @param site
 #'
 #' @return A data .table and .frame is returned.
+#'
+#' @import data.table
 #' @export
 #'
 #' @examples
 #' \dontrun{
 #' read_met("path", skip_unit = 9, skip_meta = 7)
 #' }
-read_met <- function(path = path_met, skip_unit = 9, skip_meta = 7){
+read_met <- function(path = path_met, skip_unit = 9, skip_meta = 7,
+                     startd = "2010-10-01", endd = "2012-08-01",
+                     site = "AshleyDene"){
+  start_date <- as.Date(startd)
+  end_date <- as.Date(endd)
   met_LN <- data.table::fread(input = path,skip = skip_unit, fill = TRUE)
   met_col <- read_met_col(path = path, skip = skip_meta)
   colnames(met_LN) <- colnames(met_col)
+
+  met_LN[, Clock.Today := as.Date(day, origin = paste0(year, "-01-01"))
+         ][Clock.Today > start_date & Clock.Today < end_date
+           ][, AccumTT := cumsum(mean)
+             ][, Experiment:=site]
+
   return(met_LN)
 }
